4f06ffc7aa476e69945ad20adeacc1e89dd2f578
Dmitry Gritsay <unseductable@gmail.com>
2017-03-29 06:52:40 +0300

    Add Rugexp#match? method

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,40 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
+#[macro_use] extern crate ruru;
+#[macro_use] extern crate lazy_static;
+
+extern crate regex;
+
+use regex::Regex;
+use ruru::{AnyObject, Boolean, Class, Fixnum, NilClass, Object, RString};
+
+wrappable_struct! {
+    Regex,
+    RegexWrapper,
+    REGEX_WRAPPER
+}
+
+class!(Rugexp);
+
+methods!(
+    Rugexp,
+    itself,
+
+    fn rugexp_new(pattern: RString) -> AnyObject {
+        let regex = Regex::new(&pattern.unwrap().to_str()).unwrap();
+
+        Class::from_existing("Rugexp").wrap_data(regex, &*REGEX_WRAPPER)
     }
+
+    fn rugexp_match_q(string: RString) -> Boolean {
+        let result = itself.get_data(&*REGEX_WRAPPER).is_match(string.unwrap().to_str());
+
+        Boolean::new(result)
+    }
+);
+
+pub fn define_class() {
+    Class::new("Rugexp", None).define(|itself| {
+        itself.def_self("new", rugexp_new);
+
+        itself.def("match?", rugexp_match_q);
+    });
 }
